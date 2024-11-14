c501df9873ece61d5746fac0c24ceee42cc93583
DuongThanhNghia <vongolavodecimon@gmail.com>
2024-11-14 14:29:45 +0700

    Cập nhật lại link quên mật khẩu

--- qlns/src/app/app-routing.module.ts
+++ qlns/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { EmployeeManagementComponent } from './employee-management/employee-management.component';
 import { RegisterComponent } from './register/register.component'; // Import the Register component
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', component: EmployeeManagementComponent }, // Default route for Employee Management
   { path: 'login', component: LoginComponent },          // Login route
   { path: 'register', component: RegisterComponent },    // Register route
+  { path: 'forgot-password', component: ForgotPasswordComponent }
 ];
 
 @NgModule({
