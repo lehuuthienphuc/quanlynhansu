32dc5410507e84d444bbdf3cb81f12c581cc517f
lehuuthienphuc <lehuuthienphucphuc@gmail.com>
2024-11-14 11:24:43 +0700

    cap nhat lai trang index

--- qlns/src/app/app-routing.module.ts
+++ qlns/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { EmployeeManagementComponent } from './employee-management/employee-management.component';
+import { RegisterComponent } from './register/register.component'; // Import the Register component
 
 const routes: Routes = [
-  { path: '', component: EmployeeManagementComponent }, // Default route
+  { path: '', component: EmployeeManagementComponent }, // Default route for Employee Management
   { path: 'login', component: LoginComponent },          // Login route
+  { path: 'register', component: RegisterComponent },    // Register route
 ];
 
 @NgModule({
